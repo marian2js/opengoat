5702e72e1fca5069d204185409463a768824e1dd
marian2js <marianomdq1@gmail.com>
2026-02-07 14:58:05 +0100

    feat: Add superjson and configure tRPC client to use it for data serialization.

--- packages/electron/src/renderer/src/lib/trpc.ts
+++ packages/electron/src/renderer/src/lib/trpc.ts
@@ -1,7 +1,10 @@
+import type { AppRouter } from "@main/ipc/router";
 import { createTRPCProxyClient } from "@trpc/client";
 import { ipcLink } from "electron-trpc/renderer";
-import type { AppRouter } from "@main/ipc/router";
+
+import superjson from "superjson";
 
 export const trpc = createTRPCProxyClient<AppRouter>({
-  links: [ipcLink()]
+  links: [ipcLink()],
+  transformer: superjson,
 });
