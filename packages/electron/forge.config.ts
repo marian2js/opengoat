04140d21d78697c5492af77f95f50f996f8424bf
marian2js <marianomdq1@gmail.com>
2026-02-09 20:53:00 +0100

    build(electron): Set Electron Forge configuration.

--- packages/electron/forge.config.ts
+++ packages/electron/forge.config.ts
@@ -1,42 +1,42 @@
-import { VitePlugin } from "@electron-forge/plugin-vite";
 import { MakerDeb } from "@electron-forge/maker-deb";
 import { MakerRpm } from "@electron-forge/maker-rpm";
-import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
+import { VitePlugin } from "@electron-forge/plugin-vite";
 import type { ForgeConfig } from "@electron-forge/shared-types";
 
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
-    name: "OpenGoat"
+    name: "OpenGoat",
   },
   makers: [
-    new MakerSquirrel({}),
-    new MakerZIP({}, ["darwin"]),
+    // new MakerSquirrel({}),
+    // new MakerDMG({}),
+    new MakerZIP({}, ["darwin", "win32"]),
     new MakerRpm({}),
     new MakerZIP({}, ["linux"]),
-    new MakerDeb({})
+    new MakerDeb({}),
   ],
   plugins: [
     new VitePlugin({
       build: [
         {
           entry: "src/main/main.ts",
-          config: "vite.main.config.ts"
+          config: "vite.main.config.ts",
         },
         {
           entry: "src/main/preload.ts",
-          config: "vite.preload.config.ts"
-        }
+          config: "vite.preload.config.ts",
+        },
       ],
       renderer: [
         {
           name: "main_window",
-          config: "vite.renderer.config.ts"
-        }
-      ]
-    })
-  ]
+          config: "vite.renderer.config.ts",
+        },
+      ],
+    }),
+  ],
 };
 
 export default config;
